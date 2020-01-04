f9991ff0860b9c710fecea52a210d7d266428b23
Matheus Lima Ferreira <matheuslf.17@gmail.com>
2020-01-04 11:22:20 -0300

    create project

---
+++ GHFollowers/GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesVC.swift
+//  GHFollowers
+//
+//  Created by Matheus Lima Ferreira on 1/3/20.
+//  Copyright © 2020 Matheus Lima Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollowers/GHFollowers/FollowerListVC.swift
@@ -0,0 +1,22 @@
+//
+//  FollowersListVC.swift
+//  GHFollowers
+//
+//  Created by Matheus Lima Ferreira on 1/3/20.
+//  Copyright © 2020 Matheus Lima Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    var username: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = true
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+}
